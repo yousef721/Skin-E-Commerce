409701f8f8df93751d4cce6aef118e4e733813f0
yousef721 <yousarsefabedalla255@gmail.com>
2023-10-04 17:38:30 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header/Header'
+import Hero from './Components/Hero/Hero'
+import Slider from './Components/Slider/Slider'
+import Virtual from './Components/Virtual/Virtual'
+import Products from './Components/Products/Products'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Footer from './Components/Footer/Footer'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Slider />
+      <Virtual />
+      <Products />
+      <Testimonials />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
